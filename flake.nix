e7c18420310667da01b724af253861788cbedeeb
brianaung <brianaung16@gmail.com>
2024-11-13 22:03:56 +1100

    feat: phoenix (wip)

--- flake.nix
+++ flake.nix
@@ -11,19 +11,23 @@
         {
           rust-basic = {
             path = ./rust-basic;
-            description = "A simple flake for a basic rust project.";
+            description = "Flake for a basic rust project.";
           };
           laravel = {
             path = ./laravel;
-            description = "A flake for a laravel project.";
+            description = "Flake for a laravel project.";
           };
           lua-neovim = {
             path = ./lua-neovim;
-            description = "A flake for a writing Neovim plugin in Lua.";
+            description = "Flake for a writing Neovim plugin in Lua.";
           };
           react = {
             path = ./react;
-            description = "A flake for react projects.";
+            description = "Flake for react projects.";
+          };
+          phoenix = {
+            path = ./phoenix;
+            description = "Flake for phoenix projects.";
           };
         };
       formatter.x86_64-linux = pkgs.nixpkgs-fmt;
